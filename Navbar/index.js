234c2783f912efcda73df3d33beee02271a3001f
Amitsharma45 <sharma.amit20111@gmaail.com>
2023-01-09 21:52:27 +0530

    navbar update

--- Navbar/index.js
+++ Navbar/index.js
@@ -1,8 +1,13 @@
+var t=1;
 function toggle() {
     var x = document.getElementsByClassName('list')[0];
-    if (x.style.display === 'none') {
-        x.style.display = 'flex';
+    if (t) {
+        x.classList.add('a');
+        x.classList.remove('b');
+        t=0;
     } else {
-        x.style.display = 'none';
+        t=1;
+        x.classList.add('b');
+        x.classList.remove('a');
     }
 }
